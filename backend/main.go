0f6b3df84e4d2051bdcec847eb04094ca93af347
Sanjith Devineni <sanjithdevineni@gmail.com>
2026-02-09 17:49:29 -0500

    Minor fix in backend

--- backend/main.go
+++ backend/main.go
@@ -9,16 +9,17 @@ func main() {
     mux := http.NewServeMux()
 
     // Placeholder endpoint to confirm server is running
-    mux.HandleFunc("/", func(w http.Resp>, r *http.Request) {
+    mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         w.WriteHeader(http.StatusOK)
         _, _ = w.Write([]byte("Backend running"))
     })
+
 
     server := &http.Server{
         Addr:    ":8080",
         Handler: mux,
     }
 
-    log.Println("Backend listening >)
+    log.Println("Backend listening")
     log.Fatal(server.ListenAndServe())
 }
