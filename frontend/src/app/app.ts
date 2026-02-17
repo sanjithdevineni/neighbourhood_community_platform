1d990bc08981b96af12ceb43467ada7d807d4c76
bindhusreereddy <allabindhusreereddy@gmail.com>
2026-02-17 02:32:30 -0500

    resolved merge conflicts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { AnnouncementListComponent } from './announcement-list/announcement-list.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [AnnouncementListComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  readonly title = 'Neighborhood Community';
-}
+export class App {}
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { AnnouncementListComponent } from '../../announcement-list/announcement-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [AnnouncementListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly title = 'Neighborhood Community';
+}
