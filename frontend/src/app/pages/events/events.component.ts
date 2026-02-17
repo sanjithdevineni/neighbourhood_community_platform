cf6091ba47035cea69a2f4772c066f23ff902e3b
bindhusreereddy <allabindhusreereddy@gmail.com>
2026-02-17 01:11:18 -0500

    committing events component

---
+++ frontend/src/app/pages/events/events.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  standalone: true,
+  templateUrl: './events.component.html',
+  styleUrl: './events.component.css'
+})
+export class EventsComponent {}
