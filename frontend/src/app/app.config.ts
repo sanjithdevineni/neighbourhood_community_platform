e8639c3d495f75f7e55e6732450352afa8ec8a69
bindhusreereddy <allabindhusreereddy@gmail.com>
2026-03-15 16:30:23 -0400

    fixed cors issue

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
