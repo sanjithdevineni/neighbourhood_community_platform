a2c3c2f3d81b4a434c6abc5e61bf1efa7bd26620
bindhusreereddy <allabindhusreereddy@gmail.com>
2026-03-20 22:55:19 -0400

    resolved merge conflicts
    # Conflicts:
    #	frontend/src/app/app.config.ts

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -7,7 +7,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient()
   ]
 };
